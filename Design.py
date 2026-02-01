1da1cc607da489f9208713c7ee4d0bd6350782f0
JorgeDiaz2529 <jordiaz397@gmail.com>
2026-02-01 13:17:19 -0500

    vibecode

--- Design.py
+++ Design.py
@@ -1,8 +1,27 @@
 import pygame
 
-class Design():
-    def __init__(self):
-        self.sprites = []
-        self.change_delay = 5
-        self.transition_type = None
-        self.bg_color = [0,0,0]
+class Design:
+    def __init__(self, text, bg_color, text_color=(255, 255, 255), delay=2000):
+        """
+        :param text: Text to display
+        :param bg_color: Background color (R, G, B)
+        :param text_color: Text color (R, G, B)
+        :param delay: Delay in milliseconds before switching to next design
+        """
+        self.text = text
+        self.bg_color = bg_color
+        self.text_color = text_color
+        self.delay = delay
+
+    def render(self, size, font):
+        """
+        Render the design onto its own surface.
+        """
+        surface = pygame.Surface(size).convert_alpha()
+        surface.fill(self.bg_color)
+
+        text_surface = font.render(self.text, True, self.text_color)
+        text_rect = text_surface.get_rect(center=surface.get_rect().center)
+
+        surface.blit(text_surface, text_rect)
+        return surface
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # albright_billboard
-vibecode 1 csc 142 project
+Write me two python files with each having their own classes which are:
+- Billboard
+- Design
+The Billboard class will be the main class. The billboard class will have a property called "designs" that holds a list of Design class instances. The Billboard class will have the run function that runs the program. The Billboard class will have the change_design function that cycles through the design list property on the self. The design class is a class that holds the design of the billboard class that will be visually shown on screen. The design class should have a delay integer variable that adds a delay to the billboard class change_design function. 
+Use the pygame library to show the results in a visual format.
+
+Note: ChatGPT used for vibecoding part
