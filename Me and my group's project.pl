d3636e5d53a2853f1745ff007047ce9e7ca3c38d
Aidan Smith <aidan.smith001@albright.edu>
2026-02-02 17:35:40 -0500

    Delete Me and my group's project.pl

--- Me and my group's project.pl
+++
@@ -1,5 +0,0 @@
-Me and my group's project is about showcasing a variety of different branches of Albright College through its Logos. Examples of what these 
-logos will represent are Albright's athletics program, Public Safety and the school of Albright College itself. These logos will be represented
-in the form of a slideshow; in other words, the logos will be stacked ontop of one another and move down the screen so that the viewer can see 
-every single one. There will also be backrounds for these logos consisting of red and white, representing the colors of Albright itself. That 
-is the basics of what me and my group's code will be like.
